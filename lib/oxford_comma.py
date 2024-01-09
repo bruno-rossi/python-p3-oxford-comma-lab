7237315424c1f195e07e81f2f12d4ca2213a6891
Bruno Rossi <brunogabrielrossi@gmail.com>
2024-01-09 15:46:52 -0500

    Who gives a F about an oxford comma?

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,14 @@
 def oxford_comma(items):
-    return None
+    
+    if len(items) == 1:
+        return items[0]
+    elif len(items) == 2:
+        return " and ".join(items)
+    elif len(items) == 3:
+        return f"{items[0]}, {items[1]}, and {items[2]}"
+    elif len(items) > 3:
+        items[-1] = "and " + items[-1]
+        return ", ".join(items)
+
+items = ["fiddleheads", "okra", "kohlrabi", "celery"]
+oxford_comma(items)
